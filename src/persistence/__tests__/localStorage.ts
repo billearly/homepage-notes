cc3ae9323dc9862d492835983cf68b9a2aea7810
Bill Early <earlybilly@gmail.com>
2020-01-08 23:22:09 -0500

    Notes persisted on single property instead of on top level localStorage

--- src/persistence/__tests__/localStorage.ts
+++ src/persistence/__tests__/localStorage.ts
@@ -1,53 +1,104 @@
-import { getNote, updateNote } from "../localStorage";
+import {
+  getNotes,
+  updateNote,
+  createNote
+} from "../localStorage";
+import { Note, Notes } from "../../models";
 
-const STORAGE_KEY = "NOTE";
+const NOTES_KEY = "NOTES";
 
 describe("Local Storage", () => {
-  describe("Get Note", () => {
-    beforeEach(() => {
-      window.localStorage.clear();
-    });
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
 
-    test("Should return a default note if none is found in local storage", () => {
-      const note = getNote();
+  describe("getNotes", () => {
+    test("Gets notes from localStorage", () => {
+      const storedNotes: Notes = {
+        abc123: {
+          id: "abc123",
+          creationDate: 0,
+          title: "Note 1",
+          body: "This is the first note"
+        },
+        efg456: {
+          id: "efg456",
+          creationDate: 1,
+          title: "Note 2",
+          body: "This is the second note"
+        }
+      };
 
-      expect(note.title).toBe("");
-      expect(note.body).toBe("");
-    });
+      window.localStorage.setItem(NOTES_KEY, JSON.stringify(storedNotes));
 
-    test("Should return return a note with empty title if stored value cant be parsed (v1 storage model)", () => {
-      window.localStorage.setItem(STORAGE_KEY, "this is my note");
+      const notes = getNotes();
 
-      const note = getNote();
+      const firstNote = notes["abc123"];
+      const secondNote = notes["efg456"];
 
-      expect(note.title).toBe("");
-      expect(note.body).toBe("this is my note");
-    });
+      expect(firstNote.id).toBe("abc123");
+      expect(firstNote.title).toBe("Note 1");
 
-    test("Should return a note if stored value can be parsed", () => {
-      window.localStorage.setItem(STORAGE_KEY, JSON.stringify({ title: "My Title", body: "this is the body" }));
-
-      const note = getNote();
-
-      expect(note.title).toBe("My Title");
-      expect(note.body).toBe("this is the body");
+      expect(secondNote.id).toBe("efg456");
+      expect(secondNote.title).toBe("Note 2");
     });
   });
 
-  describe("Save Note", () => {
+  describe("updateNote", () => {
     beforeEach(() => {
       window.localStorage.clear();
     });
 
-    test("Should write a note to local storage", () => {
-      updateNote({
-        title: "My Title",
-        body: "this is the body"
-      });
+    test("Should update an existing note in local storage", () => {
+      // Store an original note
+      const storedNotes: Notes = {
+        abc123: {
+          id: "abc123",
+          creationDate: 0,
+          title: "Original Title",
+          body: "Original body"
+        },
+      }
 
-      const storedValue = window.localStorage.getItem(STORAGE_KEY);
+      window.localStorage.setItem(NOTES_KEY, JSON.stringify(storedNotes));
 
-      expect(storedValue).toBe("{\"title\":\"My Title\",\"body\":\"this is the body\"}");
+      // Update the note
+      const id = "abc123";
+      const note: Note = {
+        id: id,
+        creationDate: 0,
+        title: "Updated Title",
+        body: "Updated body"
+      };
+
+      const isSuccessful = updateNote(id, note);
+      const updatedValue = window.localStorage.getItem(NOTES_KEY);
+      const updatedNotes: Notes = JSON.parse(updatedValue);
+      const updatedNote: Note = updatedNotes[id];
+
+      expect(isSuccessful).toBe(true);
+      expect(updatedNote.title).toBe("Updated Title");
+      expect(updatedNote.body).toBe("Updated body");
+    });
+  });
+
+  describe("createNote", () => {
+    test("Should add a new empty note to local storage and return it", () => {
+      const newNote = createNote();
+
+      const storedValue = window.localStorage.getItem(NOTES_KEY);
+      const storedNotes: Notes = JSON.parse(storedValue);
+      const storedNote: Note = storedNotes[newNote.id];
+
+      // Default values are correct
+      expect(storedNote.title).toBe("");
+      expect(storedNote.body).toBe("");
+
+      // Object returned matches object in localStorage
+      expect(newNote.id).toBe(storedNote.id);
+      expect(newNote.creationDate).toBe(storedNote.creationDate);
+      expect(newNote.title).toBe(storedNote.title);
+      expect(newNote.body).toBe(storedNote.body);
     });
   });
 });
--- src/persistence/localStorage.ts
+++ src/persistence/localStorage.ts
@@ -1,47 +1,57 @@
 import { Note, Notes } from "../models";
 import uuidv4 from "uuid/v4";
 
-const NOTE_KEY_PREFIX: string = "NOTE";
+const NOTES_KEY = "NOTES";
 
 export const getNotes = (): Notes => {
-  let notes: Notes = {};
-
   if (localStorage) {
-    const allStoredKeys = Object.keys(localStorage);
-    const allNoteKeys = allStoredKeys.filter(key => key.startsWith(NOTE_KEY_PREFIX));
-
-    allNoteKeys.forEach(key => {
-      const value = localStorage.getItem(key);
-      notes[key] = JSON.parse(value);
-    });
+    return getStoredNotes();
   }
 
-  return notes;
+  return {};
 }
 
 export const updateNote = (id: string, note: Note): boolean => {
   if (localStorage) {
-    localStorage.setItem(id, JSON.stringify(note));
+    let notes = getStoredNotes();
+    updateNotes(notes, id, note);
+
     return true;
   }
 
   return false;
 };
 
-// consider renaming this to createNote
 export const createNote = (): Note => {
   if (localStorage) {
     const guid: string = uuidv4();
-    const id = `${NOTE_KEY_PREFIX}_${guid}`;
 
     const newNote: Note = {
-      id: id,
+      id: guid,
       creationDate: Date.now(),
       title: "",
       body: ""
     }
 
-    localStorage.setItem(id, JSON.stringify(newNote));
+    let notes = getStoredNotes();
+    updateNotes(notes, guid, newNote);
+
     return newNote;
   }
+
+  // Handle lack of localStorage
+}
+
+const getStoredNotes = (): Notes => {
+  let notes: Notes = {};
+
+  const storedNotes = localStorage.getItem(NOTES_KEY) || JSON.stringify({});
+  notes = JSON.parse(storedNotes);
+
+  return notes;
+}
+
+const updateNotes = (notes: Notes, id: string, note: Note) => {
+  notes[id] = note;
+  localStorage.setItem(NOTES_KEY, JSON.stringify(notes));
 }
